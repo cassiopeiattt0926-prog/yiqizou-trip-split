29b27f8bdd750be7fba6bd5ba27f2d4a42553e4d
cassiopeiattt0926-prog <cassiopeiattt0926@gmail.com>
2026-08-15 11:13:43 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='yiqizou-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','style.css','app.js','manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
