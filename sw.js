85f31fb6afa2990d588d398bf8be194fd210b8ea
cassiopeiattt0926-prog <cassiopeiattt0926@gmail.com>
2026-08-15 22:06:23 +0800

    Bump 好友记 cache to v24

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='haoyouji-v20';
-const ASSETS=['./','index.html','style.css?v=20','avatars.js?v=20','app.js?v=20','manifest.webmanifest','assets/brand/tt-transfer.svg','assets/brand/app-icon-180.png','assets/brand/app-icon-192.png','assets/brand/app-icon-512.png','assets/banners/multi-currency.jpg','assets/banners/participants.jpg','assets/banners/borrow.jpg','assets/banners/settlement.jpg','assets/banners/personal-stats.jpg'];
+const C='haoyouji-v24';
+const ASSETS=['./','index.html','style.css?v=24','avatars.js?v=24','app.js?v=24','manifest.webmanifest','assets/brand/tt-transfer.svg','assets/brand/app-icon-180.png','assets/brand/app-icon-192.png','assets/brand/app-icon-512.png','assets/banners/multi-currency.jpg','assets/banners/participants.jpg','assets/banners/borrow.jpg','assets/banners/settlement.jpg','assets/banners/personal-stats.jpg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(C).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==C).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(C).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request))));
